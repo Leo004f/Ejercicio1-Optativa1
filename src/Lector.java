21e53e4fae4414a81f49efe7c679d41e0bed3f04
Leonardo <leodlf.4@gmail.com>
2023-03-15 14:50:52 -0300

    Prueba lectura del Archivo Vial.csv

--- src/Lector.java
+++ src/Lector.java
@@ -1,5 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Lector {
+	
+	private BufferedReader lector;
+	private String linea;
+	private String partes[];
+	
+	
+	public void leerArchivo(String nombre) {
+		try {
+			
+			lector = new BufferedReader(new FileReader(nombre));
+			while ((linea = lector.readLine()) != null ) {
+				partes = linea.split(",");
+				imprimir();
+				System.out.println();
+			}
+			lector.close();
+			linea = null;
+			partes = null;
+					
+		}catch(Exception e) 
+		{
+			
+		}
+	}
+	
+	public void imprimir() {
+		
+		for(int i=0; i< partes.length; i++) {
+			System.out.print(partes[i]+"-");
+		}
+		
+	}
 	
 	
 
--- src/main.java
+++ src/main.java
@@ -3,6 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
+		LecturaArchivo lectura = new LecturaArchivo();
+		
+		lectura.leerArchivo("C:\\Users\\lyons\\Downloads\\vial.csv");
 		
 
 	}
